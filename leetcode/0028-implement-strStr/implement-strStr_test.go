e3151fefbc4c620d492e4cc8bab1b214856c5722
Jonny <ji394su311203106@gmail.com>
2021-12-07 22:50:01 +0800

    0028-implement-strStr

--- leetcode/0028-implement-strStr/implement-strStr_test.go
+++ leetcode/0028-implement-strStr/implement-strStr_test.go
@@ -12,24 +12,24 @@ func TestStrStr(t *testing.T) {
 	if result != expectation {
 		t.Fatalf("expect is %d but result is %d, but fail", expectation, result)
 	}
-	//
-	//haystack = ""
-	//needle = ""
-	//
-	//result = strStr(haystack,needle)
-	//expectation = 0
-	//
-	//if result != expectation {
-	//	t.Fatalf("expect is %d but result is %d, but fail", expectation, result)
-	//}
-	//
-	//haystack = "aaaaa"
-	//needle = "bba"
-	//
-	//result = strStr(haystack,needle)
-	//expectation = -1
-	//
-	//if result != expectation {
-	//	t.Fatalf("expect is %d but result is %d, but fail", expectation, result)
-	//}
+
+	haystack = ""
+	needle = ""
+
+	result = strStr(haystack,needle)
+	expectation = 0
+
+	if result != expectation {
+		t.Fatalf("expect is %d but result is %d, but fail", expectation, result)
+	}
+
+	haystack = "aaaaa"
+	needle = "bba"
+
+	result = strStr(haystack,needle)
+	expectation = -1
+
+	if result != expectation {
+		t.Fatalf("expect is %d but result is %d, but fail", expectation, result)
+	}
 }
