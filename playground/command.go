27a796a2e9d82cd49cf1f94134fb16f5a3e1b446
Jonny <ji394su311203106@gmail.com>
2022-03-24 22:53:44 +0800

    update

---
+++ playground/command.go
@@ -0,0 +1,15 @@
+package playground
+
+import "os/exec"
+
+
+var cmdStart = (*exec.Cmd).Start
+
+type Feature struct {
+	Cmd string
+}
+
+func (f *Feature) Start() error {
+	cmd := exec.Command(f.Cmd)
+	return cmdStart(cmd)
+}
