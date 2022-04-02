7770487e6a02272762df770667b82f6c45e9f530
Jonny <ji394su311203106@gmail.com>
2022-04-02 22:39:52 +0800

    update

--- config/config.go
+++ config/config.go
@@ -1,1 +1,39 @@
 package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+const defaultPath = "./config/config.yml"
+type (
+	// Config -.
+	Config struct {
+		Redis  `json:"redis"`
+	}
+
+	// Redis -.
+	Redis struct {
+		Name string `json-required:"true" json:"name"`
+	}
+)
+// NewConfig returns app config.
+func NewConfig(configPath string) (*Config, error) {
+
+	if configPath == "" {
+		configPath = defaultPath
+	}
+	cfg := &Config{}
+
+	err := cleanenv.ReadConfig(configPath, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	err = cleanenv.ReadEnv(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
--- playground/distributed-locks/cmd/redis.go
+++ playground/distributed-locks/cmd/redis.go
@@ -2,9 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
-
 	"github.com/spf13/cobra"
+	"go-leetcode/config"
 )
 
 // redisCmd represents the redis command
@@ -12,7 +11,9 @@ var redisCmd = &cobra.Command{
 	Use:   "redis",
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(viper.AllKeys())
+		config,err := config.NewConfig(cfgFile)
+		fmt.Println(config)
+		fmt.Println(err)
 	},
 }
 
